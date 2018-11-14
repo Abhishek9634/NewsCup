21000680580715faed2325f31621c193250ebce8
Abhishek <abhishekthapliyal7579@gmail.com>
2018-11-14 18:07:44 +0530

    #1

---
+++ NewsCup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsCup
+//
+//  Created by Abhishek Thapliyal on 14/11/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
