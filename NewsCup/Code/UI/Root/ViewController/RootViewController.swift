c1a0a7cc3eb2a97543e4d2012b7b1ca0512c1c7d
Abhishek <abhishekthapliyal7579@gmail.com>
2018-12-15 14:31:52 +0530

    UI

---
+++ NewsCup/Code/UI/Root/ViewController/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  NewsCup
+//
+//  Created by Abhishek Thapliyal on 15/12/18.
+//  Copyright © 2018 Nickelfox. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
